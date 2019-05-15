d0679944ebadc7bab46cc73f84af889abf47e756
Bishwas Thapa <bishwas4@outlook.com>
2019-05-15 17:34:23 +0545

    validation added for contact

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,10 @@
 class Booking < ApplicationRecord
   
+  # Associations
   belongs_to :user
   belongs_to :package
   
+  # Valdations
+  validates :contact, required: true, length: { minimum: 6, maximum: 10}
   
 end
